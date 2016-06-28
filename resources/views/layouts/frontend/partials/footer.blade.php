8475da0afa4d789b201c103d456527e1bbfb5db8
Zack Davis <zack@octopodainteractive.com>
2016-06-28 18:06:46 -0500

    more changes from Jennifer

--- resources/views/layouts/frontend/partials/footer.blade.php
+++ resources/views/layouts/frontend/partials/footer.blade.php
@@ -4,7 +4,7 @@
 			<li><a href="https://twitter.com/mcdanielrdn"><i class="fa fa-twitter"></i></a></li>
 			<li><a href="https://www.instagram.com/mcdanielrdn/"><i class="fa fa-instagram"></i></a></li>
 			<li><a href="https://www.facebook.com/mcdanielnutrition/"><i class="fa fa-facebook"></i></a></li>
-			<li><a href="https://www.linkedin.com/in/jennifer-mcdaniel-ms-rd-cssd-ld"><i class="fa fa-linkedin"></i></a></li>
+			<li><a href="https://www.linkedin.com/in/jennifer-mcdaniel-ms-rd-cssd-ld-14a1787"><i class="fa fa-linkedin"></i></a></li>
 		</ul>
 	</div>
 
